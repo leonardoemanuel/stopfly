eee5eda3fe4740cff71112ca80caf1e002fe2491
Leonardo Emanuel <leonardodev2005@gmail.com>
2022-07-06 21:13:35 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,121 @@
+let gameBoard=document.querySelector('.gameBoard')
+let score=document.getElementById('score')
+let pontos=0
+let velocity=10
+let  time=100
+
+const aviao=document.querySelector('.alvo')
+const aviaoPosition=aviao.offsetLeft
+aviao.style.animation='none'
+
+/*-------------START WORLD----------------*/
+let sectionWorld=document.querySelector('.world')
+let divStart=document.querySelector('.divStart')
+
+function start(){
+    time=100
+    pontos=0
+    score.innerText=`SCORE:${pontos}`
+    divStart.style.display="none"
+    setTimeout(()=> {
+        aviao.style.animation=`run ${velocity}s infinite linear`
+    },1000)
+    
+    let clock=document.getElementById('time')
+    clock.innerText=`TIME:${time}`
+    clock.style.color='black'
+    const loop = setInterval(() => {
+        time-=1
+        clock.innerText=`TIME:${time}`
+        
+        if(time==0){
+            clearInterval(loop)
+        }else if(time<=10){
+            clock.style.color='#cc0505'
+        }
+    },1000)
+    const timeLoop=setInterval(()=>{
+        if(time==0){
+            divStart.style.display="FLEX"
+            aviao.style.animation='none'
+            pontos=0
+            velocity=10
+        clearInterval(timeLoop)
+        }
+    },1000)
+}
+
+function play(){
+    divStart.style.display='none'
+    sectionWorld.style.display='flex'
+}
+
+function city(){
+    gameBoard.style.backgroundImage='url(imagens/fundo-cidadex.png) no-repeat center bottom'
+    sectionWorld.style.display='none'
+    aviao.src='./imagens/aviao.png'
+}
+
+function lago(){
+    gameBoard.style.backgroundImage='url(imagens/fundo-lagox.png)'
+    sectionWorld.style.display='none'
+    aviao.src='./imagens/teco-teco.png'
+}
+
+function espaco(){
+    gameBoard.style.backgroundImage='url(imagens/fundo-espaco.jpg)'
+    sectionWorld.style.display='none'
+    aviao.src='./imagens/foguete.png'
+}
+/*--------------------------------------------------*/
+
+let bonusTime=document.getElementById('bonusTime')
+let bonusSpeed=document.getElementById('bonusSpeed')
+
+function bonusTimes(){
+    time+=50
+    bonusTime.style.display='none'
+    bonusSpeed.style.display='none'
+}
+function bonusSpeeds(){
+    velocity+=5
+    bonusTime.style.display='none'
+    bonusSpeed.style.display='none'
+}
+function acertou(){
+    if(pontos % 2 == 0){
+        aviao.style.bottom='40%'
+    }else if(pontos % 3 == 0){
+        aviao.style.bottom='20%'
+    }else(
+        aviao.style.bottom='70%'
+    )
+    if(velocity>1){
+        velocity=velocity-1
+    }else{
+        velocity=1
+    }
+    
+    if(pontos>9 && pontos % 10 == 0){
+        bonusTime.style.animation='bonus 5s'
+        bonusSpeed.style.animation='bonus 5s'
+        bonusTime.style.display='block'
+        bonusSpeed.style.display='block'
+        setTimeout(()=>{
+            
+            bonusTime.style.animation='none'
+            bonusSpeed.style.animation='none'
+        },3000)
+    }
+
+    aviao.style.animation='none'
+    setTimeout(()=> {
+        aviao.style.animation=`run ${velocity}s infinite linear`
+    },1000)
+    pontos+=1
+    score.innerText=`SCORE:${pontos}`
+}
+
+
+
+        
